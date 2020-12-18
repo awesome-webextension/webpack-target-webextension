0dc5f91fc416db5822199d041434b09e1b13c074
Jack Works <jackworks@protonmail.com>
2020-12-18 16:13:31 +0800

    chore: add example for manifest v3

---
+++ examples/extension-webpack5-manifest-v3/src/background.js
@@ -0,0 +1,6 @@
+import('./log').then(({ log }) => {
+  log('this is background script')
+  chrome.runtime.onMessage.addListener((message) => {
+    log(`receive message "${message}" from content script`)
+  })
+})
