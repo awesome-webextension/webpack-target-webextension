c1161896029599f504e6228044623108857a74cb
Jack Works <jackworks@protonmail.com>
2021-09-24 20:23:00 +0800

    test: add MV2+none

--- tests/utils/config.js
+++ tests/utils/config.js
@@ -6,9 +6,10 @@ const CopyPlugin = require('copy-webpack-plugin')
  * @param {string} folder
  * @param {string} output
  * @param {(config: import('webpack').Configuration) => void} [modify]
+ * @param {import('../../index').WebExtensionPluginOptions} [arg]
  * @returns {import('webpack').Configuration}
  */
-module.exports = (folder, output = folder.replace('fixtures', 'snapshot'), modify) => {
+module.exports = (folder, output = folder.replace('fixtures', 'snapshot'), modify, arg) => {
   const isMV3 = folder.includes('mv3')
   const manifest = join(__dirname, isMV3 ? '../fixtures/manifest-mv3.json' : '../fixtures/manifest-mv2.json')
   /** @type {import('webpack').Configuration} */
@@ -22,7 +23,7 @@ module.exports = (folder, output = folder.replace('fixtures', 'snapshot'), modif
       clean: true,
     },
     plugins: [
-      new WebExtensionPlugin({
+      new WebExtensionPlugin(arg ?? {
         background: { entry: 'background', manifest: isMV3 ? 3 : 2 },
       }),
       new CopyPlugin({
