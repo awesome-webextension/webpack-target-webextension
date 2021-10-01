55a4963b3f22bb39d985833a693d3c3051e371b2
Jack Works <jackworks@protonmail.com>
2021-10-01 13:35:35 +0800

    docs: add example for HMR in MV3

--- examples/src/hmr.js
+++ examples/src/hmr.js
@@ -1,14 +1,21 @@
 import { value } from './shared/constant.js'
 
 const isWorker = typeof importScripts === 'function'
-if (import.meta.webpackHot) {
-  const head = `[HMR test]${isWorker ? ' [Inside Worker]' : ''}`
-  if (isWorker) {
-    console.warn(head, 'HMR in Worker does not work. Help wanted.')
-  }
-  console.log(`${head} constant =`, value, '. Try edit /shared/constant.js to see changes')
+const isMV3 = chrome.runtime.getManifest().manifest_version === 3
 
+if (import.meta.webpackHot) {
   import.meta.webpackHot.accept('./shared/constant.js', () => {
     console.log(`${head} constant updated to`, value)
   })
+
+  const head = `[HMR test]${isWorker ? ' [Inside Worker]' : ''}`
+  if (isWorker) {
+    if (isMV3)
+      console.warn(
+        head,
+        'HMR for MV3 background service worker is not possible. See https://bugs.chromium.org/p/chromium/issues/detail?id=1198822'
+      )
+    else console.warn(head, 'HMR in Worker does not work. Help wanted.')
+  }
+  console.log(`${head} constant =`, value, '. Try edit /shared/constant.js to see changes')
 }
