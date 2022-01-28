70e9646e7da34cefa5ce26f1f1d4d23a943ace78
Jack Works <jackworks@protonmail.com>
2022-01-28 23:36:47 +0800

    docs: add example for react hmr (#30)
    * docs: add lockfile of examples
    
    * docs: add example for react hmr

---
+++ examples/react-hmr/webpack.config.js
@@ -0,0 +1,52 @@
+const WebExtension = require('webpack-target-webextension')
+const webpack = require('webpack')
+const { join } = require('path')
+const ReactRefreshTypeScript = require('react-refresh-typescript')
+const RefreshPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
+const ReactRefreshPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
+const isDevelopment = process.env.NODE_ENV !== 'production'
+
+/** @type {webpack.Configuration} */
+const config = {
+  devtool: 'eval-cheap-source-map',
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: {
+          loader: 'ts-loader',
+          options: {
+            transpileOnly: true,
+            getCustomTransformers: () => ({
+              before: [isDevelopment && ReactRefreshTypeScript()].filter(Boolean),
+            }),
+          },
+        },
+      },
+    ],
+  },
+  entry: {
+    background: join(__dirname, './src/background.ts'),
+    content: join(__dirname, './src/content.tsx'),
+    options: join(__dirname, './src/options.tsx'),
+  },
+  output: {
+    path: join(__dirname, './dist'),
+    // Our assets are emitted in /dist folder of our web extension.
+    publicPath: '/dist/',
+    environment: {
+      dynamicImport: true,
+    },
+  },
+  plugins: [
+    new WebExtension({ background: { entry: 'background' } }),
+    isDevelopment && new ReactRefreshPlugin(),
+  ].filter(Boolean),
+  devServer: {
+    hot: 'only',
+  },
+}
+module.exports = config
