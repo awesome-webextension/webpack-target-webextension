f2e5dafd9c71a797f8c8b7aad473e933ff62e900
Jack Works <5390719+Jack-Works@users.noreply.github.com>
2024-11-23 03:16:55 +0800

    fix: test

--- tests/ci.js
+++ tests/ci.js
@@ -2,8 +2,9 @@ const { execSync } = require('child_process')
 const { join } = require('path')
 
 const cwd = join(__dirname, '../')
-execSync('yarn test', { cwd })
+execSync('yarn test', { cwd, env: { ...process.env, CI: 'true' } })
 
-const out = execSync('git diff', { cwd, stdio: 'inherit' })
-console.log(out)
-if (out.length) throw new Error(`No files should be changed after CI test. Please update snapshot locally.`)
+if (execSync('git diff', { cwd }).length) {
+  execSync('git diff', { cwd, stdio: 'inherit' })
+  throw new Error(`No files should be changed after CI test. Please update snapshot locally.`)
+}
