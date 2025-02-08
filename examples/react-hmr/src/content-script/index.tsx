2e2377548af3a905dbeeb0eacc47da53124305d8
Jack Works <5390719+Jack-Works@users.noreply.github.com>
2025-02-08 16:45:34 +0800

    docs: example of using experimental output on content script entry

--- examples/react-hmr/src/content-script/index.tsx
+++ examples/react-hmr/src/content-script/index.tsx
@@ -1,11 +1,18 @@
 import { createRoot } from 'react-dom/client'
 import { App } from './App.tsx'
 
-if (document.readyState === 'complete') initial()
+let unmount: () => void
+import.meta.webpackHot?.accept()
+import.meta.webpackHot?.dispose(() => unmount?.())
+
+console.log('Extension running!')
+
+if (document.readyState === 'complete') unmount = initial()
 else
   document.addEventListener('readystatechange', () => {
-    if (document.readyState === 'complete') initial()
+    if (document.readyState === 'complete') unmount = initial()
   })
+
 function initial() {
   const root = document.createElement('div')
   const shadow = root.attachShadow({ mode: 'open' })
@@ -15,17 +22,15 @@ function initial() {
   shadow.adoptedStyleSheets = [style]
   fetchCSS().then((response) => style.replace(response))
 
-  if (import.meta.webpackHot) {
-    import.meta.webpackHot.accept('./style.css', () => {
-      fetchCSS().then((response) => style.replace(response))
-    })
-  }
+  import.meta.webpackHot?.accept('./style.css', () => {
+    fetchCSS().then((response) => style.replace(response))
+  })
 
-  createRoot(shadow).render(<App />)
-}
-declare global {
-  interface ImportMeta {
-    webpackHot?: any
+  const mountingPoint = createRoot(shadow)
+  mountingPoint.render(<App />)
+  return () => {
+    mountingPoint.unmount()
+    root.remove()
   }
 }
 
